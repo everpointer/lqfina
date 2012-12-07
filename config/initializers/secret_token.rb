aa3a670d3e5fb207b3b46066ad1df5db190849fb
everpointer <vane.zhangfeng@gmail.com>
2012-12-07 16:07:11 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LqFinance::Application.config.secret_token = '690d1caabc9a08cf01a6c836be7c012e164d8126aaaebc207b71bd52e7c62f7a40034f173ffaf2dfa34049246183cc4b434d97b2410e8f57a084a67bb2f3359a'
