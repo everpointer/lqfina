4f9f07f5127b306d2ad4a7e1e6baae3bf40e15d2
everpointer <vane.zhangfeng@gmail.com>
2012-12-22 10:35:50 +0800

    fix migration issue and combine some migration into table's migration.(but actually shouldn't do this, history will be gone

--- db/migrate/20121220073111_add_partner_to_partner.rb
+++
@@ -1,5 +0,0 @@
-class AddPartnerToPartner < ActiveRecord::Migration
-  def change
-    add_column :products, :partner_id, :interger
-  end
-end
--- db/migrate/20121220074708_change_partner_id_of_product.rb
+++
@@ -1,9 +0,0 @@
-class ChangePartnerIdOfProduct < ActiveRecord::Migration
-  def up
-    change_column :products, :partner_id, :interger, :null => false
-  end
-
-  def down
-    change_column :products, :partner_id, :interger
-  end
-end
