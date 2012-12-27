6772dc5b9b5225a7a54ef20eb83b5e8597252a26
everpointer <vane.zhangfeng@gmail.com>
2012-12-27 23:02:45 +0800

    fix change_refund_nums_of_group_buys settle_nums type issue

--- db/migrate/20121227072444_change_refund_nums_of_group_buy.rb
+++ db/migrate/20121227072444_change_refund_nums_of_group_buy.rb
@@ -3,16 +3,16 @@ class ChangeRefundNumsOfGroupBuy < ActiveRecord::Migration
   def up
     change_column :group_buys, :product_name, :string, :null => false
     change_column :group_buys, :settle_type, :string, :null => false, :limit => 11
-    change_column :group_buys, :settle_nums, :string, :null => false
+    change_column :group_buys, :settle_nums, :integer, :null => false
     change_column :group_buys, :refund_nums, :integer, :default => 0, :null => false
     change_column :group_buys, :state, :string, :default => '未处理', :null => false, :limit => 11
   end
 
   def down
-    t.string :product_name
-    t.string :settle_type
-    t.integer :settle_nums
-    t.integer :refund_nums
-    t.string :state
+    change_column :group_buys, :product_name, :string
+    change_column :group_buys, :settle_type, :string
+    change_column :group_buys, :settle_nums, :integer
+    change_column :group_buys, :refund_nums, :integer
+    change_column :group_buys, :state, :string
   end
 end
