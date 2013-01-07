25086289d344e287b471747b469c5c7078de26e3
everpointer <vane.zhangfeng@gmail.com>
2013-01-07 16:07:44 +0800

    add tmall_product_link to products' index page

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ProductsHelper
   def partner_collection(partners)
     collection = {}
@@ -7,5 +8,10 @@ module ProductsHelper
 
   def get_partner_name(product)
     product.partner.name unless product.partner.nil?
+  end
+
+  def tmall_product_link(product_id)
+    tmall_link = "http://detail.tmall.com/item.htm?id=#{product_id}"
+    link_to("↳", tmall_link, :target=> "_blank") 
   end
 end
