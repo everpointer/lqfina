7d2d1ca1097cd6a9dc357965afa13d1bffca01a8
everpointer <vane.zhangfeng@gmail.com>
2013-01-19 16:11:27 +0800

    add a little bit test for group_buy model and try test it with travis-ci

--- spec/models/group_buy_spec.rb
+++ spec/models/group_buy_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe GroupBuy do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:business) { GroupBuy.new(dsr: 4.3) }
+
+  its(:dsr_rate) { should eq 0.8 }
 end
