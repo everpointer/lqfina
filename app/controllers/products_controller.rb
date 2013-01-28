97373cd3c2033404d5bb896e64a8e5d8797985e2
everpointer <vane.zhangfeng@gmail.com>
2013-01-28 15:56:03 +0800

    products list show on index page in DESC order by end_date

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    @products = Product.order("end_date DESC")
 
     respond_to do |format|
       format.html # index.html.erb
